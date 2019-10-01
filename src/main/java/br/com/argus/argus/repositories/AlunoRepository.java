faf31a44134d5391090d02eadde892cab3da3f70
gbmatheus <gabrielmatheus2503@gmail.com>
2019-09-30 21:53:35 -0300

    Criaao do repository para acesso ao bd de aluno

---
+++ src/main/java/br/com/argus/argus/repositories/AlunoRepository.java
@@ -0,0 +1,9 @@
+package br.com.argus.argus.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.argus.argus.models.Aluno;
+
+public interface AlunoRepository extends JpaRepository<Aluno, Long>{
+
+}
