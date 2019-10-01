71986524608503320b10ed6e9fa7e9a890d5cd7f
gbmatheus <gabrielmatheus2503@gmail.com>
2019-09-30 22:05:19 -0300

    Informações do pkg

---
+++ src/main/java/br/com/argus/argus/dtos/package-info.java
@@ -0,0 +1,8 @@
+package br.com.argus.argus.dtos;
+/**
+* Pacote responsável pelo dto - data transfer object
+* 	Está sendo utilizado para pegar os dados da requisição e repassar para o restante da API
+* 	
+* 	- FuncionarioDto - contém os dados das entidades pessoa, usuário e funcionario
+*
+*/
---
+++ src/main/java/br/com/argus/argus/exception/package-info.java
@@ -0,0 +1,6 @@
+package br.com.argus.argus.exception;
+
+/**
+*Pacote contém as classe com exceções a serem tratadas
+*	ServicesException - tratar as exceções do pkg service, por enquanto só do findOne
+*/
