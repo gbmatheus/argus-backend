1579c4771138bcd28537e1a6e05a96333392554b
gbmatheus <gabrielmatheus2503@gmail.com>
2019-10-02 08:54:09 -0300

    Craiação do service, método save em funcionamento

--- src/main/java/br/com/argus/argus/services/PessoaService.java
+++ src/main/java/br/com/argus/argus/services/PessoaService.java
@@ -1,8 +1,12 @@
 package br.com.argus.argus.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.argus.argus.models.Endereco;
+import br.com.argus.argus.models.Pessoa;
 import br.com.argus.argus.repositories.PessoaRepository;
 
 @Service
@@ -11,4 +15,35 @@ public class PessoaService {
 	@Autowired
 	PessoaRepository pessoaRepository;
 	
+	@Autowired
+	EnderecoService enderecoService;
+	
+	public Pessoa save(Pessoa pessoaDto) {
+		Pessoa pessoa = new Pessoa();
+		pessoa.setNome(pessoaDto.getNome());
+		pessoa.setDataNascimento(pessoaDto.getDataNascimento());
+		pessoa.setNaturalidade(pessoaDto.getNaturalidade());
+		
+		pessoa.setEndereco(
+				enderecoService.save(
+						pessoaDto.getEndereco()));
+		System.out.println("Salvou enderenco ##################");
+		return pessoaRepository.save(pessoa);
+	}
+	
+	public Pessoa findById(long id) {
+		return null;
+	}
+	
+	public List<Pessoa> findByAll(){
+		return null;
+	}
+	
+	public Pessoa update(Pessoa pessoa) {
+		return null;
+	}
+	
+	public void delete(long id) {
+		
+	}
 }
