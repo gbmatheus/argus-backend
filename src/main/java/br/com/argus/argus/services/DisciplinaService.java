9c4bb344ce2036d760a2d50c760808f9fbdc7f05
gbmatheus <gabrielmatheus2503@gmail.com>
2019-10-14 20:45:25 -0300

    Criação do service

---
+++ src/main/java/br/com/argus/argus/services/DisciplinaService.java
@@ -0,0 +1,57 @@
+package br.com.argus.argus.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.argus.argus.models.Disciplina;
+import br.com.argus.argus.repositories.DisciplinaRepositories;
+
+public class DisciplinaService extends GenericService<Disciplina> {
+		
+	@Autowired
+	DisciplinaRepositories disciplinaRepositories;
+
+	@Override
+	public Optional<Disciplina> findById(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Disciplina findBy(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Disciplina> findByAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Disciplina save(Disciplina obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Disciplina update(Disciplina obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remove(Disciplina obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(long id) {
+		// TODO Auto-generated method stub
+		
+	}
+}
