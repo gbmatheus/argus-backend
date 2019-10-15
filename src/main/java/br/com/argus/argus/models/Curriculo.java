7b5e0f79018333cf1778c1a0fcdff9d22c5f0931
gbmatheus <gabrielmatheus2503@gmail.com>
2019-10-14 22:20:54 -0300

    Adicionando o relacionamento de disciplinas no currículo

--- src/main/java/br/com/argus/argus/models/Curriculo.java
+++ src/main/java/br/com/argus/argus/models/Curriculo.java
@@ -18,34 +18,41 @@ import br.com.argus.argus.enums.AnoLetivo;
 @Entity
 @Table(name = "curriculos")
 public class Curriculo {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
+
 	@Column(length = 10, nullable = false)
 	private String codigo;
-	
+
 	@Column(length = 50, nullable = false)
 	private String nome;
-	
+
 	@Column(name = "ano_letivo", nullable = false)
 	@Enumerated(EnumType.STRING)
 	private AnoLetivo anoLetivo;
-	
+
 	@OneToMany
 	@JoinColumn(name = "curriculos_id")
 	private List<Turma> turma;
-	
+
+	@OneToMany
+	@JoinColumn(name = "curriculos_id")
+	private List<Disciplina> disciplinas;
+
 	public Curriculo() {
 	}
 
-	public Curriculo(Long id, String codigo, String nome, AnoLetivo anoLetivo, List<Turma> turma) {
+	public Curriculo(Long id, String codigo, String nome, AnoLetivo anoLetivo, List<Turma> turma,
+			List<Disciplina> disciplinas) {
+		super();
 		this.id = id;
 		this.codigo = codigo;
 		this.nome = nome;
 		this.anoLetivo = anoLetivo;
 		this.turma = turma;
+		this.disciplinas = disciplinas;
 	}
 
 	public Long getId() {
@@ -82,6 +89,14 @@ public class Curriculo {
 
 	public void setTurma(List<Turma> turma) {
 		this.turma = turma;
+	}
+
+	public List<Disciplina> getDisciplinas() {
+		return disciplinas;
+	}
+
+	public void setDisciplinas(List<Disciplina> disciplinas) {
+		this.disciplinas = disciplinas;
 	}
 
 }
