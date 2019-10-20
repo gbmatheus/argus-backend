888148e2cdbe0c9af4892398dce7fdfc11e7d84f
gbmatheus <gabrielmatheus2503@gmail.com>
2019-10-20 17:51:15 -0300

    Criação dos services

---
+++ src/main/java/br/com/argus/argus/services/CurriculoService.java
@@ -0,0 +1,5 @@
+package br.com.argus.argus.services;
+
+public class CurriculoService {
+
+}
---
+++ src/main/java/br/com/argus/argus/services/TurmaService.java
@@ -0,0 +1,49 @@
+package br.com.argus.argus.services;
+
+import java.util.List;
+import java.util.Optional;
+
+public class TurmaService extends GenericService<TurmaService>{
+
+	@Override
+	public Optional<TurmaService> findById(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TurmaService findBy(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TurmaService> findByAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TurmaService save(TurmaService obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TurmaService update(long id, TurmaService obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remove(long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(long id) {
+		// TODO Auto-generated method stub
+		
+	}
+}
