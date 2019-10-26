fdbcd02741cf8e0535f8e118f1a9e5601783cb20
gbmatheus <gabrielmatheus2503@gmail.com>
2019-10-25 22:39:45 -0300

    Create e index ok. Controller responsavel por chamar cada regra de negócio dos objetos envolvidos.

--- src/main/java/br/com/argus/argus/controllers/FuncionarioController.java
+++ src/main/java/br/com/argus/argus/controllers/FuncionarioController.java
@@ -8,30 +8,92 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import br.com.argus.argus.exception.ValidarColunaException;
+import br.com.argus.argus.models.Endereco;
 import br.com.argus.argus.models.Funcionario;
+import br.com.argus.argus.models.Pessoa;
+import br.com.argus.argus.models.Usuario;
 import br.com.argus.argus.responses.Response;
+import br.com.argus.argus.services.EndService;
+import br.com.argus.argus.services.EnderecoService;
 import br.com.argus.argus.services.FuncionarioService;
+import br.com.argus.argus.services.PeService;
+import br.com.argus.argus.services.PessoaService;
+import br.com.argus.argus.services.UService;
+import br.com.argus.argus.services.UsuarioService;
 
+@Validated
 @RestController
 @RequestMapping("/api/funcionarios")
 public class FuncionarioController {
-	
+
+	@Autowired
+	private EnderecoService enderecoService;
+
+	@Autowired
+	private PessoaService pessoaService;
+
+	@Autowired
+	private UsuarioService usuarioService;
+
 	@Autowired
 	private FuncionarioService funcionarioService;
-	
+
+	@Transactional
+	@PostMapping
+	@ResponseBody
+	public ResponseEntity<Response<Funcionario>> create(@Valid @RequestBody Funcionario funcionarioDto,
+			BindingResult result) {
+		
+		Response<Funcionario> response = new Response<Funcionario>();
+		
+		if (result.hasErrors()) {
+			result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+			return ResponseEntity.badRequest().body(response);
+		}
+
+		Endereco endereco = funcionarioDto.getPessoa().getEndereco();
+		endereco = enderecoService.save(endereco);
+		System.out.println("Passou aki");
+		System.out.println(endereco.toString());
+
+		Pessoa pessoa = funcionarioDto.getPessoa();
+		System.out.println("Passou aki 1");
+		System.out.println(pessoa.toString());
+//		pessoa.setEndereco(enderecoService.save(funcionarioDto.getPessoa().getEndereco()));
+//		pessoa.setEndereco(endereco);
+		pessoa = pessoaService.save(pessoa);
+		System.out.println("Passou aki 2");
+
+		Usuario usuario = usuarioService.save(funcionarioDto.getUsuario());
+		System.out.println(usuario.toString());
+
+		Funcionario funcionario = new Funcionario();
+		funcionario.setPessoa(pessoa);
+		funcionario.setUsuario(usuario);
+		funcionario.setCargaHoraria(funcionarioDto.getCargaHoraria());
+		funcionario.setCpf(funcionarioDto.getCpf());
+		System.out.println(funcionarioDto.toString());
+		System.out.println(funcionario.toString());
+
+		Funcionario obj;
+		obj = this.funcionarioService.save(funcionario);
+		response.setData(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(funcionario.getId())
+				.toUri();
+		return ResponseEntity.created(uri).body(response);
+
+	}
 
 	@GetMapping
 	@ResponseBody
@@ -39,67 +101,14 @@ public class FuncionarioController {
 		List<Funcionario> funcionarios = funcionarioService.findByAll();
 		return ResponseEntity.status(HttpStatus.OK).body(funcionarios);
 	}
-
-	@GetMapping(value = "/{id}")
-	@ResponseBody
-	public ResponseEntity<Response<Funcionario>> show(@PathVariable("id") long id) {
-		Funcionario funcionario = funcionarioService.findBy(id);
-		Response<Funcionario> response = new Response<Funcionario>();
-		response.setData(funcionario);
-		return ResponseEntity.status(HttpStatus.OK).body(response);
-	}
-
-	@PostMapping
-	@ResponseBody
-	public ResponseEntity<Response<Funcionario>> create(@Valid @RequestBody Funcionario funcionario, BindingResult result) {
-		Response<Funcionario> response = new Response<Funcionario>();
-		
-		
-		if (result.hasErrors()) {
-			result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
-			return ResponseEntity.badRequest().body(response);
-		}
-				
-		Funcionario obj;
-		try {
-			obj = this.funcionarioService.save(funcionario);
-			response.setData(obj);
-		} catch (ValidarColunaException e) {
-			e.printStackTrace();
-		}
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(funcionario.getId()).toUri();
-		return ResponseEntity.created(uri).body(response);
-
-	}
-
-	@PutMapping(value = "/{id}")
-	@ResponseBody
-	public ResponseEntity<Response<Funcionario>> update(@PathVariable("id") long id, @RequestBody Funcionario funcionario) {
-//		Response<Funcionario> response = new Response<Funcionario>();
 //
-//		return funcionarioService.findById(id).map(record -> {
-//			Funcionario updated = funcionarioService.save(funcionario);
-//			response.setData(updated);
-//			return ResponseEntity.accepted().body(response);
-//		}).orElse(ResponseEntity.notFound().build());
-		return ResponseEntity.notFound().build();
+//	@GetMapping(value = "/{id}")
+//	@ResponseBody
+//	public ResponseEntity<Response<Funcionario>> show(@PathVariable("id") long id) {
+//		Funcionario funcionario = funcionarioService.findBy(id);
+//		Response<Funcionario> response = new Response<Funcionario>();
+//		response.setData(funcionario);
+//		return ResponseEntity.status(HttpStatus.OK).body(response);
+//	}
 
-	}
-
-	@DeleteMapping(path = "/{id}")
-	@ResponseBody
-	public void remove(@PathVariable("id") long id) {
-
-	}
-
-	@DeleteMapping(path = "/delete/{id}")
-	@ResponseBody
-	public ResponseEntity<?> delete(@PathVariable long id) {
-		return funcionarioService.findById(id).map(record -> {
-			funcionarioService.deleteById(id);
-			return ResponseEntity.ok().build();
-		}).orElse(ResponseEntity.notFound().build());
-
-	}
-	
 }
