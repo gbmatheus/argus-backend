b2e9199780ee9855ec4ca153c7c3f0540cd9b95e
gbmatheus <gabrielmatheus2503@gmail.com>
2019-10-25 22:35:32 -0300

    Extendendo do ServiceGeneric

--- src/main/java/br/com/argus/argus/services/EnderecoService.java
+++ src/main/java/br/com/argus/argus/services/EnderecoService.java
@@ -1,98 +1,68 @@
 package br.com.argus.argus.services;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.argus.argus.models.Endereco;
 import br.com.argus.argus.repositories.EnderecoRepository;
 
 @Service
-public class EnderecoService extends GenericService<Endereco>{
-	
+public class EnderecoService extends ServiceGeneric<Endereco> {
+
 	@Autowired
 	EnderecoRepository enderecoRepository;
-	
-	
-	public Endereco save(Endereco enderecoDto) {
-		Endereco endereco = new Endereco();
-		
-		for (Endereco e : findByAll()) {
-			if(e.getRua().equalsIgnoreCase(enderecoDto.getRua())
-					&& e.getBairro().equalsIgnoreCase(enderecoDto.getBairro())
-					&& e.getCidade().equalsIgnoreCase(enderecoDto.getCidade())
-					&& e.getUf().equalsIgnoreCase(enderecoDto.getUf())
-					&& e.getCep().equalsIgnoreCase(e.getCep())
-					) {
-				endereco = e;
-				endereco.setNumero(e.getNumero());
-				return enderecoRepository.save(endereco);
-				
-			}else if(e.getRua().equalsIgnoreCase(enderecoDto.getRua())
-					&& e.getNumero() == enderecoDto.getNumero()
-					&& e.getBairro().equalsIgnoreCase(enderecoDto.getBairro())
-					&& e.getCidade().equalsIgnoreCase(enderecoDto.getCidade())
-					&& e.getUf().equalsIgnoreCase(enderecoDto.getUf())
-					&& e.getCep().equalsIgnoreCase(e.getCep())
-					){
-				System.out.println("Endereco id "+e.getId());
-				return e;
+
+	@Override
+	public JpaRepository<Endereco, Long> getRepository() {
+		return enderecoRepository;
+	}
+
+	@Override
+	@Transactional
+	public Endereco save(Endereco objetoDto) {
+		List<Endereco> enderecos = findByAll();
+
+		if (enderecos.size() != 0) {
+			for (Endereco e : enderecos) {
+				if (e.getRua().equalsIgnoreCase(objetoDto.getRua())
+						&& e.getBairro().equalsIgnoreCase(objetoDto.getBairro())
+						&& e.getCidade().equalsIgnoreCase(objetoDto.getCidade())
+						&& e.getUf().equalsIgnoreCase(objetoDto.getUf()) && e.getCep().equalsIgnoreCase(e.getCep())) {
+					Endereco endereco = e;
+					endereco.setNumero(e.getNumero());
+					return super.save(e);
+				} else if (e.getRua().equalsIgnoreCase(objetoDto.getRua()) && e.getNumero() == objetoDto.getNumero()
+						&& e.getBairro().equalsIgnoreCase(objetoDto.getBairro())
+						&& e.getCidade().equalsIgnoreCase(objetoDto.getCidade())
+						&& e.getUf().equalsIgnoreCase(objetoDto.getUf()) && e.getCep().equalsIgnoreCase(e.getCep())
+						&& e.getNumero() == e.getNumero()) {
+					System.out.println("Endereco id " + e.getId());
+					return e;
+				}
 			}
 		}
-		
-		endereco.setRua(enderecoDto.getRua());
-		endereco.setNumero(enderecoDto.getNumero());
-		endereco.setBairro(enderecoDto.getBairro());
-		endereco.setCidade(enderecoDto.getCidade());
-		endereco.setUf(enderecoDto.getUf());
-		endereco.setCep(enderecoDto.getCep());
-		
-		return enderecoRepository.save(endereco);
+
+		return super.save(objetoDto);
 	}
 
 	@Override
-	public Optional<Endereco> findById(long id) {
-		return enderecoRepository.findById(id);
-	}
-
-	@Override
-	public Endereco findBy(long id) {
-//		return enderecoRepository.findOne(id);
-		return null;
-	}
-
-	@Override
-	public List<Endereco> findByAll() {
-		return enderecoRepository.findAll();
-	}
-
-
-	@Override
+	@Transactional
 	public Endereco update(long id, Endereco enderecoDto) {
-		return findById(id)
-				.map(record-> {
-					record.setRua(enderecoDto.getRua());
-					record.setNumero(enderecoDto.getNumero());
-					record.setBairro(enderecoDto.getBairro());
-					record.setCidade(enderecoDto.getCidade());
-					record.setUf(enderecoDto.getUf());
-					record.setCep(enderecoDto.getCep());
-					
-					Endereco endereco = enderecoRepository.save(record);
-					return endereco;
-				}).orElse(null);
-	}
+		return findById(id).map(record -> {
+			record.setRua(enderecoDto.getRua());
+			record.setNumero(enderecoDto.getNumero());
+			record.setBairro(enderecoDto.getBairro());
+			record.setCidade(enderecoDto.getCidade());
+			record.setUf(enderecoDto.getUf());
+			record.setCep(enderecoDto.getCep());
 
-	@Override
-	public void deleteById(long id) {
-		
-	}
-
-	@Override
-	public void remove(long id) {
-		
+			Endereco endereco = enderecoRepository.save(record);
+			return endereco;
+		}).orElse(null);
 	}
 
 }
--- src/main/java/br/com/argus/argus/services/PessoaService.java
+++ src/main/java/br/com/argus/argus/services/PessoaService.java
@@ -1,103 +1,62 @@
 package br.com.argus.argus.services;
 
 import java.util.List;
-import java.util.Optional;
-
-import javax.persistence.EntityExistsException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import br.com.argus.argus.exception.ServicesException;
-import br.com.argus.argus.exception.ValidarColunaException;
 import br.com.argus.argus.models.Pessoa;
 import br.com.argus.argus.repositories.PessoaRepository;
 
 @Service
-public class PessoaService extends GenericService<Pessoa> {
-
-	@Autowired
-	EnderecoService enderecoService;
+public class PessoaService extends ServiceGeneric<Pessoa> {
 
 	@Autowired
 	PessoaRepository pessoaRepository;
 
-	public Pessoa save(Pessoa pessoaDto) throws EntityExistsException, ValidarColunaException {
+	@Override
+	public JpaRepository<Pessoa, Long> getRepository() {
+		return pessoaRepository;
+	}
+	
+	@Override
+	@Transactional
+	public Pessoa save(Pessoa objetoDto) {
 		List<Pessoa> pessoas = findByAll();
-		Pessoa pessoa = new Pessoa();
-
-//		if (!pessoaDto.getRg().matches("^[0-9]*$"))
-//			throw new ValidarColunaException("Rg inválido");
 
 		if (pessoas.size() != 0) {
 			for (Pessoa p : pessoas) {
-				if (p.getNome().equalsIgnoreCase(pessoaDto.getNome())
-						&& p.getNaturalidade().equalsIgnoreCase(p.getNaturalidade())
-						&& p.getRg().equalsIgnoreCase(pessoaDto.getRg())
-						&& p.getDataNascimento().getTime() == pessoaDto.getDataNascimento().getTime()
-						&& p.getEndereco().equals(pessoaDto.getEndereco()))
+				if (p.getNome().equalsIgnoreCase(objetoDto.getNome())
+						&& p.getNaturalidade().equalsIgnoreCase(objetoDto.getNaturalidade())
+						&& p.getRg().equalsIgnoreCase(objetoDto.getRg())
+						&& p.getDataNascimento().getTime() == objetoDto.getDataNascimento().getTime()
+						&& p.getEndereco().equals(objetoDto.getEndereco()))
 					return p;
-
-				else if (p.getRg().equalsIgnoreCase(pessoaDto.getRg())) {
-					throw new EntityExistsException("Já existe uma pessoa com esse rg");
-				}
+				
+				else if(p.getRg().equalsIgnoreCase(objetoDto.getRg()))
+					return null;//retornar erro
 			}
 		}
 
-		pessoa.setNome(pessoaDto.getNome());
-		pessoa.setDataNascimento(pessoaDto.getDataNascimento());
-		pessoa.setNaturalidade(pessoaDto.getNaturalidade());
-		pessoa.setRg(pessoaDto.getRg());
-
-		pessoa.setEndereco(enderecoService.save(pessoaDto.getEndereco()));
-
-		return pessoaRepository.save(pessoa);
-
+		return super.save(objetoDto);
 	}
-
-	public Optional<Pessoa> findById(long id) {
-		return pessoaRepository.findById(id);
-	}
-
-	public List<Pessoa> findByAll() {
-		return pessoaRepository.findAll();
-	}
-
+	
 	@Override
-	public Pessoa findBy(long id) {
-		Pessoa pessoa = pessoaRepository.findOne(id);
-		if (pessoa == null)
-			throw new ServicesException("Usuário não está cadastrado");
-
-		return pessoa;
-
-	}
-
-	@Override
-	public Pessoa update(long id, Pessoa pessoaDto) {
+	@Transactional
+	public Pessoa update(long id, Pessoa objetoDto) {
 		return findById(id).map(record -> {
-			record.setNome(pessoaDto.getNome());
-			record.setDataNascimento(pessoaDto.getDataNascimento());
-			record.setNaturalidade(pessoaDto.getNaturalidade());
-			record.setRg(pessoaDto.getRg());
-			record.setEndereco(pessoaDto.getEndereco());
+			record.setNome(objetoDto.getNome());
+			record.setDataNascimento(objetoDto.getDataNascimento());
+			record.setNaturalidade(objetoDto.getNaturalidade());
+			record.setRg(objetoDto.getRg());
+			record.setEndereco(objetoDto.getEndereco());
 
 			Pessoa pessoa = pessoaRepository.save(record);
 			return pessoa;
 		}).orElse(null);
 	}
 
-	@Override
-	public void remove(long id) {
-		findById(id).map(record -> {
-			record.setAtivo(!record.isAtivo());
-			return pessoaRepository.save(record);
 
-		}).orElse(null);
-	}
-
-	@Override
-	public void deleteById(long id) {
-		pessoaRepository.deleteById(id);
-	}
 }
