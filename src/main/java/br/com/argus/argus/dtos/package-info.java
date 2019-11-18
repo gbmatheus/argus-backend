142f2659752949b0c6098e93ceae5e65597d7881
gbmatheus <gabrielmatheus2503@gmail.com>
2019-11-18 17:29:18 -0300

    Retirando o spring security como dependência

--- src/main/java/br/com/argus/argus/dtos/package-info.java
+++
@@ -1,8 +0,0 @@
-package br.com.argus.argus.dtos;
-/**
-* Pacote responsável pelo dto - data transfer object
-* 	Está sendo utilizado para pegar os dados da requisição e repassar para o restante da API
-* 	
-* 	- FuncionarioDto - contém os dados das entidades pessoa, usuário e funcionario
-*
-*/
--- src/main/java/br/com/argus/argus/exception/EntityNotExistException.java
+++
@@ -1,7 +0,0 @@
-package br.com.argus.argus.exception;
-
-public class EntityNotExistException extends Exception{
-	
-	public EntityNotExistException(String msg) {}
-
-}
