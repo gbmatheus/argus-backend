a10bd9c10e40d7a38195630b15b98db616926a73
gbmatheus <gabrielmatheus2503@gmail.com>
2019-11-20 13:21:57 -0300

    Retirada da matricula

--- src/main/java/br/com/argus/argus/services/MatriculaService.java
+++ src/main/java/br/com/argus/argus/services/MatriculaService.java
@@ -1,61 +1,52 @@
 package br.com.argus.argus.services;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
-import br.com.argus.argus.models.Aluno;
-import br.com.argus.argus.models.Matricula;
-import br.com.argus.argus.models.Turma;
-import br.com.argus.argus.repositories.MatriculaRepository;
-
 @Service
-public class MatriculaService extends ServiceGeneric<Matricula> {
+public class MatriculaService {// extends ServiceGeneric<Matricula> {
+//
+//	@Autowired
+//	PessoaService pessoaService;
+//
+//	@Autowired
+//	AlunoService alunoService;
+//
+//	@Autowired
+//	FuncionarioService funcionarioService;
+//
+//	@Autowired
+//	TurmaService turmaService;
+//
+//	@Autowired
+//	MatriculaRepository matriculaRepository;
+//
+//	@Override
+//	public JpaRepository<Matricula, Long> getRepository() {
+//		return matriculaRepository;
+//	}
+//
+//	@Override
+//	public Matricula save(Matricula objetoDto) {
+//		return super.save(objetoDto);
+//	}
 
-	@Autowired
-	PessoaService pessoaService;
-
-	@Autowired
-	AlunoService alunoService;
-
-	@Autowired
-	FuncionarioService funcionarioService;
-
-	@Autowired
-	TurmaService turmaService;
-
-	@Autowired
-	MatriculaRepository matriculaRepository;
-
-	@Override
-	public JpaRepository<Matricula, Long> getRepository() {
-		return matriculaRepository;
-	}
-
-	@Override
-	public Matricula save(Matricula objetoDto) {
-		return super.save(objetoDto);
-	}
-
-	public Matricula registerMatricula(Long turmaID, Long alunoID) {
-		Matricula matricula = new Matricula();
-		Optional<Aluno> aluno = alunoService.findById(alunoID);
-//		Optional<Funcionario> professor = funcionarioService.findById(professorID);
-		Optional<Turma> turma = turmaService.findById(turmaID);
-
-		matricula.setAluno(aluno.get());
-		aluno.get().getMatriculas().add(matricula);
-
-//		matricula.setProfessor(professor.get());
-//		professor.get().getMatriculas().add(matricula);
-
-		matricula.setTurma(turma.get());
-		turma.get().getMatriculas().add(matricula);
-
-		return matricula;
-
-	}
+//	public Matricula registerMatricula(Long turmaID, Long alunoID) {
+//		Matricula matricula = new Matricula();
+//		Optional<Aluno> aluno = alunoService.findById(alunoID);
+////		Optional<Funcionario> professor = funcionarioService.findById(professorID);
+//		Optional<Turma> turma = turmaService.findById(turmaID);
+//
+//		matricula.setAluno(aluno.get());
+//		aluno.get().getMatriculas().add(matricula);
+//
+////		matricula.setProfessor(professor.get());
+////		professor.get().getMatriculas().add(matricula);
+//
+//		matricula.setTurma(turma.get());
+//		turma.get().getMatriculas().add(matricula);
+//
+//		return matricula;
+//
+//	}
 
 }
