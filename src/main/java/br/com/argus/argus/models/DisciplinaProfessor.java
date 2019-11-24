01748ab9d91496f104f5c9a9db5fb2c9d8651386
gbmatheus <gabrielmatheus2503@gmail.com>
2019-11-24 19:22:25 -0300

    Adicionando notas

--- src/main/java/br/com/argus/argus/models/DisciplinaProfessor.java
+++ src/main/java/br/com/argus/argus/models/DisciplinaProfessor.java
@@ -1,9 +1,13 @@
 package br.com.argus.argus.models;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -13,6 +17,8 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import br.com.argus.argus.enums.Situacao;
 
 @Entity
 @Table(name = "dis_pro")
@@ -37,6 +43,31 @@ public class DisciplinaProfessor {
 	@ManyToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "professor_id")
 	private Funcionario professor;
+
+	@Column(name = "nota_1", precision = 2, scale = 2, nullable = false)
+	private BigDecimal nota1 = new BigDecimal(0);
+
+	@Column(name = "nota_2", precision = 2, scale = 2, nullable = false)
+	private BigDecimal nota2 = new BigDecimal(0);
+
+	@Column(name = "nota_3", precision = 2, scale = 2, nullable = false)
+	private BigDecimal nota3 = new BigDecimal(0);
+
+	@Column(name = "nota_4", precision = 2, scale = 2, nullable = false)
+	private BigDecimal nota4 = new BigDecimal(0);
+
+	@Column(name = "media_geral", precision = 2, scale = 2, nullable = false)
+	private BigDecimal mediaGeral = new BigDecimal(0);
+
+	@Column(name = "prova_final", precision = 2, scale = 2, nullable = false)
+	private BigDecimal provaFinal = new BigDecimal(0);
+
+	@Column(name = "media_final", precision = 2, scale = 2, nullable = false)
+	private BigDecimal mediaFinal = new BigDecimal(0);
+
+	@Column(length = 20, nullable = false)
+	@Enumerated(EnumType.STRING)
+	private Situacao situacao = Situacao.EA;
 
 	public DisciplinaProfessor() {
 	}
@@ -67,6 +98,70 @@ public class DisciplinaProfessor {
 
 	public void setTurmas(List<Turma> turmas) {
 		this.turmas = turmas;
+	}
+
+	public BigDecimal getNota1() {
+		return nota1;
+	}
+
+	public void setNota1(BigDecimal nota1) {
+		this.nota1 = nota1;
+	}
+
+	public BigDecimal getNota2() {
+		return nota2;
+	}
+
+	public void setNota2(BigDecimal nota2) {
+		this.nota2 = nota2;
+	}
+
+	public BigDecimal getNota3() {
+		return nota3;
+	}
+
+	public void setNota3(BigDecimal nota3) {
+		this.nota3 = nota3;
+	}
+
+	public BigDecimal getNota4() {
+		return nota4;
+	}
+
+	public void setNota4(BigDecimal nota4) {
+		this.nota4 = nota4;
+	}
+
+	public BigDecimal getMediaGeral() {
+		return mediaGeral;
+	}
+
+	public void setMediaGeral(BigDecimal mediaGeral) {
+		this.mediaGeral = mediaGeral;
+	}
+
+	public BigDecimal getProvaFinal() {
+		return provaFinal;
+	}
+
+	public void setProvaFinal(BigDecimal provaFinal) {
+		this.provaFinal = provaFinal;
+	}
+
+	public BigDecimal getMediaFinal() {
+		return mediaFinal;
+	}
+
+	public void setMediaFinal(BigDecimal mediaFinal) {
+		this.mediaFinal = mediaFinal;
+	}
+
+	public Situacao getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(Situacao situacao) {
+		this.situacao = situacao;
 	}
 
 }
